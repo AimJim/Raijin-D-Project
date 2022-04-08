f5aca981b14cffa45ef85574b6ba2ed6b527cc0e
AimJim <92112507+AimJim@users.noreply.github.com>
2022-04-08 10:41:37 +0200

    La IA va más wachi

--- Raijin-D/Assets/Scripts/EnemyController.cs
+++ Raijin-D/Assets/Scripts/EnemyController.cs
@@ -5,34 +5,70 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    NavMeshAgent navMeshAgent;
+   
+    //IA propia
     [SerializeField]
-    Transform[] waypoints;
-
-
+    GameObject[] checkPoints;
+    [SerializeField]
+    float acceleration;
+    [SerializeField]
+    float maxSpeed;
+    [SerializeField]
+    float steer;
+    [SerializeField]
+    float maxAngle;
+    [SerializeField]
+    float downforce;
     
+    Rigidbody carRB;
 
-    int currentWaypoint = 0;
+    bool canAccelerate = true;
 
+    int currentCheckPoint = 0;
     private void Awake()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        
-
-        navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+        carRB = GetComponent<Rigidbody>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        if (canAccelerate)
         {
-            currentWaypoint++;
-
-
-            if (currentWaypoint >= waypoints.Length)
-            {
-                currentWaypoint = 0;
-            }
-            navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+            Accelerate();
         }
+        LimitSpeed();
+        Rotate();
+        DownForce();
+    }
+
+    private void Rotate()
+    {
+        
+       
+        transform.LookAt(checkPoints[currentCheckPoint].transform);
+    }
+    void DownForce()
+    {
+        carRB.AddForce(-transform.up * downforce);
+    }
+    private void Accelerate()
+    {
+        carRB.velocity += transform.forward * acceleration;
+    }
+    private void LimitSpeed()
+    {
+        if (carRB.velocity.magnitude > maxSpeed)
+        {
+            canAccelerate = false;
+        }
+        else
+        {
+            canAccelerate = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentCheckPoint++;
+        currentCheckPoint = currentCheckPoint % checkPoints.Length;
     }
 }
