ff386985b7a73806a8b232c0f434d096d78dea6b
AimJim <92112507+AimJim@users.noreply.github.com>
2022-05-20 19:44:22 +0200

    Radio de Locos

--- Raijin-D/Assets/Scripts/RadioController.cs
+++ Raijin-D/Assets/Scripts/RadioController.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RadioController : MonoBehaviour
 {
     [SerializeField]
     AudioClip[] songList;
+    [SerializeField]
+    GameObject canvas;
+    [SerializeField]
+    TMP_Text text;
 
     AudioSource audioSource;
 
-    public int currentSong = 0;
+    int currentSong = 0;
+    float waitTime = 2f;
 
     private void Awake()
     {
+        DontDestroyOnLoad(gameObject);
         audioSource = GetComponent<AudioSource>();
         currentSong = Random.Range(0, songList.Length);
         audioSource.clip = songList[currentSong];
+        text.text = songList[currentSong].name;
     }
 
     void Update()
@@ -26,9 +34,19 @@ public class RadioController : MonoBehaviour
             audioSource.clip = songList[currentSong];
             audioSource.Play();
             currentSong = (currentSong + 1) % songList.Length;
+           
+            StartCoroutine(showRadioUI());
             
 
         }
+    }
+
+    IEnumerator showRadioUI()
+    {
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(waitTime);
+        canvas.SetActive(false);
+        text.text = songList[currentSong].name;
     }
 
     
